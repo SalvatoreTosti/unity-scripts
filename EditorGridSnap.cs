51e0b62eb6334587580f4b5667439e2bee116816
Salvatore Tosti <saltosti@gmail.com>
2016-06-14 16:23:35 -0700

    Added in-editor snap script
    This script forces objects to snap to the nearest vertex while being manipulated in the editor.

---
+++ EditorGridSnap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EditorGridSnap : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.Ceil (transform.position.x);
+		float y = Mathf.Ceil (transform.position.y);
+		float z = Mathf.Ceil (transform.position.z);
+		transform.position = new Vector3 (x, y, z);
+	}
+}
+
+
