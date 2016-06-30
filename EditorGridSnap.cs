ef6229a341b0051dbc4f87cc31dabb423ee0d556
Salvatore Tosti <saltosti@gmail.com>
2016-06-29 21:13:29 -0700

    Made a small change to EditorGridSnap.cs
    Also added some documentation in README.

--- EditorGridSnap.cs
+++ EditorGridSnap.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class EditorGridSnap : MonoBehaviour {
-	
-	// Update is called once per frame
+
 	void Update () {
-		float x = Mathf.Ceil (transform.position.x);
-		float y = Mathf.Ceil (transform.position.y);
-		float z = Mathf.Ceil (transform.position.z);
+		float x = Mathf.Round (transform.position.x);
+		float y = Mathf.Round (transform.position.y);
+		float z = Mathf.Round (transform.position.z);
 		transform.position = new Vector3 (x, y, z);
 	}
+
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # unity-scripts
 A storage place for commonly used Unity scripts
+----
+## EditorGridSnap.cs
+
+_Overview:_
+
+Snaps object to the nearest x, y and z coordinates.
+
+_Usage:_
+
+Attach the script to any object.
+Object movement in the editor will then be snapped to the nearest whole x,y,z coordinate.
