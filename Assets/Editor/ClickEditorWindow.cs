8135282f0193f87383c48a61cfc5c32d00ed1b82
Salvatore Tosti <saltosti@gmail.com>
2016-07-03 19:40:24 -0700

    Added ClickEditorWindow script and changed directory layout
    Added a script which creates an EditorWindow that can track click events in the Scene editor.
    Also changed directory structure to better reflect how scripts should be used.

---
+++ Assets/Editor/ClickEditorWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class ClickEditorWindow : EditorWindow {
+	[MenuItem("Window/Click Tracking Window")]
+	public static void ShowWindow(){
+		EditorWindow.GetWindow (typeof(ClickEditorWindow));
+	}
+
+	void OnSceneGUI(SceneView scene)
+	{
+		if (Event.current.type == EventType.mouseDown) {
+			Vector3 mouseClick = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition).origin;
+			Debug.Log ("Click: " + mouseClick);
+		}
+	}
+
+	void OnEnable()
+	{
+		SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+	}
+	void OnDisable()
+	{
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+	} 
+}
