54a5d62761b5634e069de895c54c8eb67cf67567
Salvatore Tosti <saltosti@gmail.com>
2016-07-04 17:33:00 -0700

    Added auto-parent script for objects.

---
+++ Assets/Scripts/EditorAutoParent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EditorAutoParent : MonoBehaviour {
+	public string ParentName;
+	// Use this for initialization
+	void Start () {
+		transform.SetParent (GameObject.Find (ParentName).transform);
+	}
+
+}
