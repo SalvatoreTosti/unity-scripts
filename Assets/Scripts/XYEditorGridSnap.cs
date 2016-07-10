faea390a381beeccc06838a307f83da1ffbc8ac3
Salvatore Tosti <saltosti@gmail.com>
2016-07-10 16:32:08 -0700

    Added snapping for X and Y axis, which allows object to be placed at an arbitrary Z location.

---
+++ Assets/Scripts/XYEditorGridSnap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class XYEditorGridSnap : MonoBehaviour {
+
+	void Update () {
+		float x = Mathf.Round (transform.position.x);
+		float y = Mathf.Round (transform.position.y);
+		float z = transform.position.z;
+		transform.position = new Vector3 (x, y, z);
+	}
+}
