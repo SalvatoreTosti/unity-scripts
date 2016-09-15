210fd2bdbcf7b3d25c3ba6dcdc57a2b2b8bd8400
Salvatore Tosti <saltosti@gmail.com>
2016-09-14 18:04:02 -0700

    Added object level Look-at and Move-toward scripts

---
+++ Assets/Scripts/MoveToward.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToward : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	public float maxDistance;
+
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		Debug.Log (Vector3.Distance (transform.position, target.position));
+		if (Vector3.Distance (transform.position, target.position) > maxDistance) {
+			step = (speed * 2) * Time.deltaTime;
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+
+
+	}
+}
