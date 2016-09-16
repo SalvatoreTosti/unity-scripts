94b98e724344adf25ddc899499c92681026a2380
Salvatore Tosti <saltosti@gmail.com>
2016-09-15 17:22:10 -0700

    Added basic movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 1.0f;
+	public float rotateSpeed = 1.0f;
+	CharacterController controller;
+
+	void Start(){
+		controller = GetComponent<CharacterController> ();
+	}
+
+
+	void Update () {
+		Vector3 forwardMovement = Input.GetAxis ("Vertical") * transform.forward*moveSpeed;
+		controller.Move (forwardMovement);
+		Vector3 rotation = Input.GetAxis ("Horizontal") * transform.up * rotateSpeed;
+		transform.Rotate (rotation);
+	}
+}
