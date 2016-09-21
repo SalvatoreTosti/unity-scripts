ea692839a27db80faf5686cb8a51cabfc37c03f7
Salvatore Tosti <saltosti@gmail.com>
2016-09-20 19:33:18 -0700

    Added generic pooling mechanism
    ObjectPooler.cs provides generic pooling functionality and GameObjectPooler.cs provides an example implementation.

---
+++ Assets/Scripts/Pooler/GameObjectPooler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjectPooler : ObjectPooler<GameObject> {
+
+	public GameObject defaultObj;
+	public bool instantiateOnEmpty;
+	public bool initializeOnStart;
+
+	void Start(){
+		if (initializeOnStart && maxSize > 0) {
+			for (int i = 0; i < maxSize; i++) {
+				GameObject obj = Instantiate (defaultObj);
+				PutObject (obj);
+			}
+		}
+	}
+
+	public override void PutObject (GameObject obj)
+	{
+		if (queue == null) {
+			queue = new Queue<GameObject> ();
+		}
+		if (queue.Count >= maxSize) {
+			return;
+		} else {
+			
+			queue.Enqueue (obj);
+		}
+	}
+
+	protected override GameObject DefaultObject(){
+		if (instantiateOnEmpty) {
+			return Instantiate (defaultObj);
+		} else {
+			return null;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Pooler/ObjectPooler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class ObjectPooler<T> : MonoBehaviour {
+
+	public Queue<T> queue;
+	public bool populateOnStart;
+	public int maxSize;
+
+	public T GetObject(){
+		if (queue == null) {
+			queue = new Queue<T> ();
+		}
+
+		if (queue.Count == 0) {
+			return DefaultObject ();
+		} else {
+			return queue.Dequeue ();
+		}
+	}
+
+	public virtual void PutObject(T obj){
+		if (queue == null) {
+			queue = new Queue<T> ();
+		}
+		queue.Enqueue (obj);
+	}
+
+	public int Size(){
+		if (queue == null) {
+			queue = new Queue<T> ();
+		}
+		return queue.Count;
+	}
+
+	public void Clear(){
+		if (queue == null) {
+			queue = new Queue<T> ();
+		}
+		queue.Clear ();
+	}
+
+	protected abstract T DefaultObject ();
+}
