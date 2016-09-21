971967ca4f22d593dcfb8c7edf3e22ea75cb1d5f
Salvatore Tosti <saltosti@gmail.com>
2016-09-21 16:03:36 -0700

    Added object level gizmo drawing

---
+++ Assets/Scripts/DrawSphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawSphere : MonoBehaviour {
+
+	public Transform target;
+	public float size = 1.0f;
+	public Color gizmoColor = Color.green;
+
+	void OnDrawGizmos(){
+		Gizmos.color = gizmoColor;
+		drawSphereGizmo ();
+	}
+
+	private void drawSphereGizmo(){
+		if (target != null) {
+			Gizmos.DrawWireSphere (target.position,size);
+		}
+	}
+}
