18bcbe093289cd6c6ac6a8d5aae57f5b8edc16e1
Salvatore Tosti <saltosti@gmail.com>
2016-09-24 09:16:28 -0700

    Added scripts for smooth movement and look-at behavior for cameras

---
+++ Assets/Scripts/Camera/CameraLookBehavior.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraLookBehavior : MonoBehaviour {
+
+	public enum LookStyle { SMOOTH, LINEAR };
+
+	public float rotationSpeed = 5.0f;
+	public LookStyle lookRotation = LookStyle.SMOOTH;
+
+	public Transform defaultTarget;
+	public Transform target;
+
+	void Start(){
+		target = defaultTarget;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (lookRotation == LookStyle.SMOOTH) {
+			smoothLookAt ();
+		} else {
+			linearLookAt ();
+		}
+	}
+
+	public void AddTarget(Transform newTarget){
+		if (newTarget == null) {
+			AddTarget (defaultTarget);
+		} else {
+			target = newTarget;
+		}
+	}
+
+	public void RemoveTarget(){
+		target = defaultTarget;
+	}
+
+	private void smoothLookAt(){
+		//http://forum.unity3d.com/threads/smooth-look-at.26141/
+		Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+		// Smoothly rotate towards the target point.
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+	}
+
+	private void linearLookAt(){
+		Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+		//Linearly rotate towards the target point.
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/Camera/CameraMoveBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraMoveBehavior : MonoBehaviour {
+
+	public Transform defaultTarget;
+	public Transform target;
+
+	public float speed;
+	public float maxDistance;
+
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		if (Vector3.Distance (transform.position, target.position) > maxDistance) {
+			step = (speed * 2) * Time.deltaTime;
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+
+	public void AddTarget(Transform newTarget){
+		if (newTarget == null) {
+			AddTarget (defaultTarget);
+		} else {
+			target = newTarget;
+		}
+	}
+
+	public void RemoveTarget(){
+			target = defaultTarget;
+	}
+}
