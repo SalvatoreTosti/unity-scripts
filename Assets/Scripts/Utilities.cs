7e00e0697f087037a7d61f7a32c68f7c3be74cab
Salvatore Tosti <saltosti@gmail.com>
2016-10-01 07:06:49 -0700

    Updated Utilities.cs
    Now includes a 'nearest' collider function and a 'reactivate' / 'deactivate' pair of functions.

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -64,4 +64,41 @@ public class Utilities : MonoBehaviour {
 		return getDescendentObjects (parent);
 	}
 
+	public static Collider nearestCollider(Collider[] colliders){
+		if (colliders == null) {
+			Debug.Log ("Null collider array passed");
+			return null;
+		}
+		if (colliders.Length == 0) {
+			Debug.Log ("Empty collider array passed");
+			return null;
+		}
+
+		Collider nearest = colliders [0];
+		float minDistance = Vector3.Distance (nearest.transform.position, transform.position);
+		foreach (Collider collider in colliders) {
+			float distance = Vector3.Distance (collider.transform.position, transform.position);
+			if (distance < minDistance) {
+				nearest = collider;
+				minDistance = distance;
+			}
+		}
+		return nearest;
+	}
+
+	public static void Reactivate(GameObject obj, Vector3 position, Vector3 force){
+		obj.transform.position = position;
+		obj.SetActive (true);
+		Rigidbody rigidBody = obj.GetComponent<Rigidbody> ();
+		if (rigidBody != null) {
+			rigidBody.AddForce (force);
+		} else {
+			Debug.Log ("Attempted to apply force to an object without Rigidbody");
+		}
+	}
+
+	public static void Deactivate(GameObject obj){
+		obj.SetActive (false);
+	}
+
 }
