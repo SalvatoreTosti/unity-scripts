041e633b43c57ae9bf98c14b79a6e6733df5c94c
Salvatore Tosti <saltosti@gmail.com>
2016-11-22 19:55:11 -0800

    Added functionality for spell effects which modify AI behavior

---
+++ Assets/Scripts/Spells/Effect Types/BehaviorEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu (menuName = "Spells/Spell Effect/Behavior Effect")]
+public class BehaviorEffect : SpellEffect {
+
+	public float duration;
+	public BehaviorController.BEHAVIOR behavior;
+
+	[HideInInspector] public BehaviorController behaviorController;
+
+	public override void Initialize (GameObject caster, GameObject target)
+	{
+		behaviorController = target.GetComponent<BehaviorController> ();
+
+	
+	}
+
+	public override IEnumerator Trigger ()
+	{
+		if (behaviorController != null) {
+			behaviorController.Trigger(behavior);
+			yield return new WaitForSeconds (duration);
+			behaviorController.Trigger(BehaviorController.BEHAVIOR.NONE);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Spells/Mock Helpers/BehaviorController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviorController : MonoBehaviour {
+
+	public enum BEHAVIOR {
+		NONE,
+		FEAR
+	};
+
+	public BEHAVIOR currentBehavior;
+
+	public void Trigger(BEHAVIOR behavior){
+		currentBehavior = behavior;
+	}
+}
