a95829b3850920fbb209b929265ccae8e49ee761
Salvatore Tosti <saltosti@gmail.com>
2016-11-28 16:08:16 -0800

    Fixed bug in OTEffect implementation
    OTEffects no longer attempt to calculate a final value, then tick toward it.
    OTEffects now dynamically apply effects over a given interval.

--- Assets/Scripts/Spells/Effect Types/OTEffectFixed.cs
+++ Assets/Scripts/Spells/Effect Types/OTEffectFixed.cs
@@ -4,76 +4,85 @@ using System.Collections;
 [CreateAssetMenu (menuName = "Spells/Spell Effect/OT Effect Fixed")]
 public class OTEffectFixed : SpellEffect
 {
-
-	public float amount;
-	public float duration;
-	public float delay;
-	public bool buff;
+	public int amount;
+	public int duration;
+	public int ticksPerSecond;
 
 	public Stats.STAT_TYPE statType;
 	public SpellEffect.EFFECT_TYPE effectType;
 
-	private float tickTime;
-	private int tickNumber;
-	private float tickAmount;
-	private float endAmount;
-
 	[HideInInspector] public Stats stats;
 
-	public override void Initialize (GameObject caster, GameObject target)
-	{
+	public override void Initialize(GameObject caster, GameObject target){
 		stats = target.GetComponent<Stats> ();
-		if (stats != null) {
-			if (buff) {
-				endAmount = (float) stats.GetStat (statType) + amount;
-			} else {
-				endAmount = (float) stats.GetStat (statType) - amount;
-			}
-			endAmount = Mathf.Round (endAmount);
-
-			if (duration == 0) {
-				tickNumber = 1;
-				tickAmount = amount;
-				tickTime = 0;
-			} else {
-				tickNumber = (int)(30.0f * duration); // base assumption of 30 ticks a second
-				tickAmount = amount / tickNumber;
-				tickTime = duration / tickNumber;
-			}
+		if (stats == null) {
+			Debug.Log ("Attempted to trigger OTEffect on object with no Stats: " + target.name);
+			return;
 		}
 	}
 
-	public override IEnumerator Trigger ()
-	{
-		if (stats != null) {
-			float currentAmount = (float) stats.GetStat (statType);
-			yield return new  WaitForSeconds (delay);
-			for (int i = 0; i < tickNumber; i++) {
-				if (stats != null) {
-					currentAmount = ProtectedAdjustment (buff, currentAmount, endAmount, tickAmount);
-					stats.SetStat (statType, (int) currentAmount);
-				}
-				yield return new WaitForSeconds (tickTime);
-			}
-			stats.SetStat (statType, (int) endAmount); //final adjustment to remove any remaining small amounts
-		}
+	public override IEnumerator Trigger (){
+		OTEffectUtils.EffectTicker ticker = 
+			new OTEffectUtils.EffectTicker (amount, duration, ticksPerSecond);
+		return ticker.ApplyTicks (stats, statType);
 	}
 
-	private float ProtectedAdjustment (bool isBuff, float current, float final, float adjustment)
-	{
-		if (isBuff) {
-			if (current + adjustment > final) {
-				return final;
-			} else {
-				return current + adjustment;
-			}
-		} else {
-			if (current - adjustment < final) {
-				return final;
-			} else {
-				return current - adjustment;
-			}
-		}
-	}
+
+
+//	public override void Initialize (GameObject caster, GameObject target)
+//	{
+//		stats = target.GetComponent<Stats> ();
+//		if (stats != null) {
+//			if (buff) {
+//				endAmount = (float) stats.GetStat (statType) + amount;
+//			} else {
+//				endAmount = (float) stats.GetStat (statType) - amount;
+//			}
+//			endAmount = Mathf.Round (endAmount);
+//
+//			if (duration == 0) {
+//				tickNumber = 1;
+//				tickAmount = amount;
+//				tickTime = 0;
+//			} else {
+//				tickNumber = (int)(30.0f * duration); // base assumption of 30 ticks a second
+//				tickAmount = amount / tickNumber;
+//				tickTime = duration / tickNumber;
+//			}
+//		}
+//	}
+//
+//	public override IEnumerator Trigger ()
+//	{
+//		if (stats != null) {
+//			float currentAmount = (float) stats.GetStat (statType);
+//			yield return new  WaitForSeconds (delay);
+//			for (int i = 0; i < tickNumber; i++) {
+//				if (stats != null) {
+//					currentAmount = ProtectedAdjustment (buff, currentAmount, endAmount, tickAmount);
+//					stats.SetStat (statType, (int) currentAmount);
+//				}
+//				yield return new WaitForSeconds (tickTime);
+//			}
+//			stats.SetStat (statType, (int) endAmount); //final adjustment to remove any remaining small amounts
+//		}
+//	}
+//
+//	private float ProtectedAdjustment (bool isBuff, float current, float final, float adjustment)
+//	{
+//		if (isBuff) {
+//			if (current + adjustment > final) {
+//				return final;
+//			} else {
+//				return current + adjustment;
+//			}
+//		} else {
+//			if (current - adjustment < final) {
+//				return final;
+//			} else {
+//				return current - adjustment;
+//			}
+//		}
+//	}
 		
 }
---
+++ Assets/Scripts/Spells/Effect Types/OTEffectUtils.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OTEffectUtils : MonoBehaviour {
+
+	public class EffectTicker{
+
+		public Queue<int> tickQueue;
+		public float tickDuration;
+
+		public EffectTicker(int totalAmount, int seconds){
+			int[] ticks = GetTickArray(totalAmount, totalAmount);
+			tickDuration = (float) seconds / (float)totalAmount;
+			tickQueue = new Queue<int>(ticks);
+		}
+
+		public EffectTicker(int totalAmount, int seconds, int ticksPerSecond){
+			int[] ticks;
+
+			int absAmount = Mathf.Abs(totalAmount);
+			if(absAmount < seconds * ticksPerSecond){
+				ticks = GetTickArray(absAmount, totalAmount);
+				tickDuration = (float) seconds / (float)totalAmount;
+			} else {
+				tickDuration = (float) seconds / (float) (seconds * ticksPerSecond);
+				ticks = GetTickArray(seconds * ticksPerSecond, totalAmount);
+			}
+			tickQueue = new Queue<int>(ticks);
+		}
+
+		public bool HasNext(){
+			return tickQueue.Count > 0;
+		}
+
+		public int Next(){
+			return (int) tickQueue.Dequeue ();
+		}
+
+		/// <summary>
+		/// Returns internal tick structure.
+		/// For testing ONLY.
+		/// </summary>
+		/// <returns>Array corresponding to internal queue structure.</returns>
+		public int[] DumpTicks(){
+			return tickQueue.ToArray ();
+		}
+
+
+		public IEnumerator ApplyTicks(Stats stats, Stats.STAT_TYPE targetStat){
+			while (HasNext ()) {
+				stats.Apply (targetStat, Next());
+				yield return new WaitForSeconds (tickDuration);
+			}
+		}
+			
+	}
+
+	/// <summary>
+	/// Gets the tick array.
+	/// </summary>
+	/// <returns>The tick array.</returns>
+	/// <param name="tickNumber">Tick number.</param>
+	/// <param name="totalDamage">Total damage.</param>
+	public static int[] GetTickArray(int tickNumber, int totalDamage){
+		if (tickNumber == 0) {
+			return new int[0];
+		}
+		if (totalDamage == 0) {
+			return new int[0];
+		}
+			
+		int remainder = GetRemainder(tickNumber,totalDamage);
+		int regularTickAmount = GetTickAmount (tickNumber, totalDamage);
+		List<int> tickList = new List<int> ();
+		for (int i = 0; i < tickNumber; i++) {
+			tickList.Add (regularTickAmount);
+		}
+		tickList.Add (remainder);
+		tickList.TrimExcess();
+		return tickList.ToArray ();
+
+	}
+
+	/// <summary>
+	/// Gets the remainder.
+	/// </summary>
+	/// <returns>The remainder.</returns>
+	/// <param name="tickNumber">Tick number.</param>
+	/// <param name="totalDamage">Total damage.</param>
+	public static int GetRemainder(int tickNumber, int totalDamage){
+		if (tickNumber == 0) {
+			return 0;
+		}
+		return totalDamage % tickNumber;
+	}
+
+	/// <summary>
+	/// Gets the tick amount.
+	/// </summary>
+	/// <returns>The tick amount.</returns>
+	/// <param name="tickNumber">Tick number.</param>
+	/// <param name="totalDamage">Total damage.</param>
+	public static int GetTickAmount(int tickNumber, int totalDamage){
+		if (tickNumber == 0) {
+			return 0;
+		}
+		return totalDamage / tickNumber;
+	}
+}
